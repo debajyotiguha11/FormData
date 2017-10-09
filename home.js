4d44e31e39f7aad47ae1ecfe23dd1ead548b9f94
pankaj <pkjkumar05@gmail.com>
2017-10-10 02:08:22 +0530

    This is the simple form example

---
+++ home.js
@@ -0,0 +1,32 @@
+function handleSubmit(){
+console.log("hello 2");
+var fname = document.getElementById("fname").value; 
+var lname = document.getElementById("lname").value; 
+/* this is for taking the value from checkbox */
+var checkinput = document.getElementsByName("fooditem");
+var ckecklen = checkinput.length;
+var i;
+var inputarray = [];
+var inputarrayrad = [];
+	for(i=0;i<ckecklen;i++){
+		if(checkinput[i].checked){
+		inputarray[i]=checkinput[i].value;
+        document.getElementById("checkboxdta").innerHTML = inputarray.join(" ");
+
+		}
+	}
+/* this is for taking the value from radio button */
+var radioBtn = document.getElementsByName("modeOfPay");
+var radiolen = radioBtn.length;
+document.getElementById("firstdata").innerHTML = fname;
+document.getElementById("seconddata").innerHTML = lname;
+
+var j;
+    for(j=0;j<radiolen;j++){
+		if(radioBtn[j].checked){
+		inputarrayrad[j]=radioBtn[j].value;
+        document.getElementById("radiobtndta").innerHTML = inputarrayrad.join(" ");
+		}
+    }
+
+}
